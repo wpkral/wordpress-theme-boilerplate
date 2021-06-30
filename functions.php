077bb39d0ab2fd0d0a243daa030fd6886469b309
Emre Can ÖZTAŞ <me@emrecanoztas.com>
2021-06-30 20:24:06 +0300

    update

--- functions.php
+++ functions.php
@@ -10,3 +10,8 @@
  * @license     http://opensource.org/licenses/MIT  MIT License
  * @version     1.0
  */
+
+$theme_dir = get_template_directory();
+$theme_uri = get_template_directory_uri();
+$footer_text = '';
+$post_length = 32;
